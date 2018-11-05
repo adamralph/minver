7f77e766569a01f11acdf2c6893c3a7e304b6ced
Adam Ralph <adam@adamralph.com>
2018-11-05 23:17:57 +0100

    move verbose setting parsing to CLI

--- MinVer.Cli/Program.cs
+++ MinVer.Cli/Program.cs
@@ -15,7 +15,7 @@ namespace MinVer.Cli
             var majorMinor = app.Option("-m|--major-minor <MAJOR.MINOR>", "The MAJOR.MINOR version range. E.g. '2.0'.", CommandOptionType.SingleValue);
             var path = app.Option("-p|--path <PATH>", "The path of the repository.", CommandOptionType.SingleValue);
             var tagPrefix = app.Option("-t|--tag-prefix <TAG_PREFIX>", "The tag prefix.", CommandOptionType.SingleValue);
-            var verbose = app.Option("-v|--verbose", "Enable verbose logging.", CommandOptionType.NoValue);
+            var verbose = app.Option<bool>("-v|--verbose <VERBOSE>", "Verbose logging.", CommandOptionType.SingleValue);
 
             app.OnExecute(() =>
             {
@@ -44,7 +44,7 @@ namespace MinVer.Cli
                     }
                 }
 
-                Console.Out.WriteLine(Versioner.GetVersion(path.Value() ?? ".", verbose.HasValue(), tagPrefix.Value(), major, minor, buildMetadata.Value()));
+                Console.Out.WriteLine(Versioner.GetVersion(path.Value() ?? ".", verbose.ParsedValue, tagPrefix.Value(), major, minor, buildMetadata.Value()));
             });
 
             app.Execute(args);

a527af408b58e5a602a25eddc3fe9a64ca30ee88
Adam Ralph <adam@adamralph.com>
2018-11-24 22:40:51 +0100

    green: add MinVerTests.BuildMetadata.NoTag

--- MinVerTests/BuildMetadata.cs
+++ MinVerTests/BuildMetadata.cs
@@ -14,6 +14,21 @@ namespace MinVerTests
     public static class BuildMetadata
     {
         [Scenario]
+        [Example(default, "0.0.0-alpha.0")]
+        [Example("a", "0.0.0-alpha.0+a")]
+        public static void NoTag(string buildMetadata, string expectedVersion, string path, Repository repo, Version actualVersion)
+        {
+            $"Given a git repository with a commit in '{path = GetScenarioDirectory($"build-metadata-no-tag-{buildMetadata}")}'"
+                .x(c => repo = EnsureEmptyRepositoryAndCommit(path).Using(c));
+
+            $"When the version is determined using build metadata '{buildMetadata}'"
+                .x(() => actualVersion = Versioner.GetVersion(repo, default, default, buildMetadata, new TestLogger()));
+
+            $"Then the version is '{expectedVersion}'"
+                .x(() => Assert.Equal(expectedVersion, actualVersion.ToString()));
+        }
+
+        [Scenario]
         [Example("1.2.3+a", default, "1.2.3+a")]
         [Example("1.2.3", "b", "1.2.3+b")]
         [Example("1.2.3+a", "b", "1.2.3+a.b")]

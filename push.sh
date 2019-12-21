315ac581989f40fade67d337fab97aedecdd0991
Adam Ralph <adam@adamralph.com>
2019-12-21 12:51:49 +0000

    wip

--- push.sh
+++ push.sh
@@ -7,6 +7,6 @@ then
 else
   for package in $(find -name "*.nupkg" | grep "test" -v); do
     echo "${0##*/}: Pushing $package..."
-    dotnet nuget push $package --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key $1
+    dotnet nuget push $package --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key ${MYGET_ADAMRALPH_CI_API_KEY}
   done
 fi

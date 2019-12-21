c7076e0e6dded4aff8194ca1cd5565e3c2d432e1
Adam Ralph <adam@adamralph.com>
2019-12-21 12:48:27 +0000

    wip

--- push.sh
+++ push.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 if [[ -z "${MYGET_ADAMRALPH_CI_API_KEY}" ]]
 then
-  echo "${0##*/}": MYGET_ADAMRALPH_CI_API_KEY is empty or not set. Skipped pushing package(s).
+  echo "${0##*/}: MYGET_ADAMRALPH_CI_API_KEY is empty or not set. Skipped pushing package(s)."
 else
   for package in $(find -name "*.nupkg" | grep "test" -v); do
-    echo "${0##*/}": Pushing $package...
+    echo "${0##*/}: Pushing $package..."
     dotnet nuget push $package --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key $1
   done
 fi

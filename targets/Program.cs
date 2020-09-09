af4ef6183132d535eed6f45af990668d847b252f
Adam Ralph <adam@adamralph.com>
2020-09-09 08:23:06 +0200

    upgrade CI SDK's

--- targets/Program.cs
+++ targets/Program.cs
@@ -53,7 +53,7 @@ internal static class Program
                 var source = Path.GetFullPath("./MinVer/bin/Release/");
                 var version = Path.GetFileNameWithoutExtension(Directory.EnumerateFiles(source, "*.nupkg").First()).Split("MinVer.", 2)[1];
 
-                await RunAsync("dotnet", "new globaljson --sdk-version 3.1.302", testProject);
+                await RunAsync("dotnet", "new globaljson --sdk-version 3.1.402", testProject);
                 await RunAsync("dotnet", "new classlib", testProject);
                 await RunAsync("dotnet", $"add package MinVer --source {source} --version {version} --package-directory packages", testProject);
                 await RunAsync("dotnet", $"restore --source {source} --packages packages", testProject);

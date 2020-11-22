31198858b1bc7c79cf6b467a87abe1b8762b52c8
Adam Ralph <adam@adamralph.com>
2020-11-23 00:08:53 +0100

    read Git in tests instead of running it to suppress output

--- MinVerTests/Infra/Git.cs
+++ MinVerTests/Infra/Git.cs
@@ -14,7 +14,7 @@ namespace MinVerTests.Infra
             Commit(path);
         }
 
-        public static void Commit(string path) => Run("git", "commit -m '.' --allow-empty", path);
+        public static void Commit(string path) => Read("git", "commit -m '.' --allow-empty", path);
 
         public static void EnsureEmptyRepository(string path)
         {
@@ -23,27 +23,27 @@ namespace MinVerTests.Infra
             PrepareForCommits(path);
         }
 
-        public static void Init(string path) => Run("git", "init", path);
+        public static void Init(string path) => Read("git", "init", path);
 
         public static void PrepareForCommits(string path)
         {
-            Run("git", "config user.email johndoe@tempuri.org", path);
-            Run("git", "config user.name John Doe", path);
-            Run("git", "config commit.gpgsign false", path);
+            Read("git", "config user.email johndoe@tempuri.org", path);
+            Read("git", "config user.name John Doe", path);
+            Read("git", "config commit.gpgsign false", path);
         }
 
         public static Task<string> GetGraph(string path) => ReadAsync("git", "log --graph --pretty=format:'%d'", path);
 
-        internal static void Tag(string path, string tag) => Run("git", $"tag {tag}", path);
+        internal static void Tag(string path, string tag) => Read("git", $"tag {tag}", path);
 
-        internal static void Tag(string path, string tagName, string sha) => Run("git", $"tag {tagName} {sha}", path);
+        internal static void Tag(string path, string tagName, string sha) => Read("git", $"tag {tagName} {sha}", path);
 
-        internal static void AnnotatedTag(string path, string tag, string message) => Run("git", $"tag {tag} -a -m '{message}'", path);
+        internal static void AnnotatedTag(string path, string tag, string message) => Read("git", $"tag {tag} -a -m '{message}'", path);
 
         internal static IEnumerable<string> GetCommitShas(string path) =>
             Read("git", "log --pretty=format:\"%H\"", path, noEcho: true)
                 .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-        internal static void Checkout(string path, string sha) => Run("git", $"checkout {sha}", path);
+        internal static void Checkout(string path, string sha) => Read("git", $"checkout {sha}", path);
     }
 }

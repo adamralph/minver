fb3cb4d0a40dc3f8d50b96f8037a09c47bb2472d
Adam Ralph <adam@adamralph.com>
2022-02-01 20:21:16 +0100

    add option masking tests

--- MinVerTests.Infra/MinVerCli.cs
+++ MinVerTests.Infra/MinVerCli.cs
@@ -7,12 +7,12 @@ namespace MinVerTests.Infra
 {
     public static class MinVerCli
     {
-        public static async Task<Result> ReadAsync(string workingDirectory, string configuration = Configuration.Current, params (string, string)[] envVars)
+        public static async Task<Result> ReadAsync(string workingDirectory, string configuration = Configuration.Current, string args = "", params (string, string)[] envVars)
         {
             var environmentVariables = envVars.ToDictionary(envVar => envVar.Item1, envVar => envVar.Item2, StringComparer.OrdinalIgnoreCase);
             _ = environmentVariables.TryAdd("MinVerVerbosity".ToAltCase(), "trace");
 
-            return await CommandEx.ReadLoggedAsync("dotnet", $"exec {GetPath(configuration)}", workingDirectory, environmentVariables).ConfigureAwait(false);
+            return await CommandEx.ReadLoggedAsync("dotnet", $"exec {GetPath(configuration)} {args}", workingDirectory, environmentVariables).ConfigureAwait(false);
         }
 
         public static string GetPath(string configuration) =>

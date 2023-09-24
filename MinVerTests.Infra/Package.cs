20e6cbd8e5f571a5613c407c0538caf93063c1b9
Adam Ralph <adam@adamralph.com>
2023-09-24 15:53:19 +0200

    add SourceLink test

--- MinVerTests.Infra/Package.cs
+++ MinVerTests.Infra/Package.cs
@@ -6,15 +6,16 @@ namespace MinVerTests.Infra
 {
     public record Package(string Version, AssemblyVersion AssemblyVersion, FileVersion FileVersion)
     {
-        public static Package WithVersion(int major, int minor, int patch, IEnumerable<string>? preReleaseIdentifiers = null, int height = 0, string buildMetadata = "")
+        public static Package WithVersion(int major, int minor, int patch, IEnumerable<string>? preReleaseIdentifiers = null, int height = 0, string buildMetadata = "", string? informationalVersionAdditionalBuildMetadata = "")
         {
             var preReleaseToken = preReleaseIdentifiers == null ? "" : GetPreReleaseToken(preReleaseIdentifiers.ToList());
             var heightToken = height == 0 ? "" : $".{height}";
             var buildMetadataToken = string.IsNullOrEmpty(buildMetadata) ? "" : $"+{buildMetadata}";
 
             var version = $"{major}.{minor}.{patch}{preReleaseToken}{heightToken}{buildMetadataToken}";
+            var informationalVersion = $"{version}{informationalVersionAdditionalBuildMetadata}";
 
-            return new Package(version, new AssemblyVersion(major, 0, 0, 0), new FileVersion(major, minor, patch, 0, version));
+            return new Package(version, new AssemblyVersion(major, 0, 0, 0), new FileVersion(major, minor, patch, 0, informationalVersion));
         }
 
         private static string GetPreReleaseToken(IReadOnlyList<string> preReleaseIdentifiers) => preReleaseIdentifiers.Any() ? $"-{string.Join(".", preReleaseIdentifiers)}" : "";
---
+++ MinVerTests.Packages/SourceLink.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using MinVerTests.Infra;
+using Xunit;
+
+namespace MinVerTests.Packages;
+
+public static class SourceLink
+{
+    [Fact]
+    public static async Task HasCommitSha()
+    {
+        // arrange
+        var path = MethodBase.GetCurrentMethod().GetTestDirectory();
+        await Sdk.CreateProject(path);
+
+        _ = await Sdk.DotNet($"add package Microsoft.SourceLink.GitHub --version 1.1.1 --package-directory packages", path);
+        _ = await Sdk.DotNet("restore --packages packages", path);
+
+        await Git.Init(path);
+        await Git.Commit(path);
+        var sha = (await Git.GetCommitShas(path)).Single();
+
+        var envVars = ("MinVerBuildMetadata", "build.123");
+        var expected = Package.WithVersion(0, 0, 0, new[] { "alpha", "0", }, 0, $"build.123", $".{sha}");
+
+        // act
+        var (actual, _, _) = await Sdk.BuildProject(path, envVars: envVars);
+        var (cliStandardOutput, _) = await MinVerCli.ReadAsync(path, envVars: envVars);
+
+        // assert
+        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Version, cliStandardOutput.Trim());
+    }
+}

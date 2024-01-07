9e62f0b16d1a40229cbdfd3f84e726f429df3a62
Your Name <you@example.com>
2024-01-07 19:23:25 +0100

    fixed expected values for OutputVariables.cs

--- MinVerTests.Packages/OutputVariables.cs
+++ MinVerTests.Packages/OutputVariables.cs
@@ -14,22 +14,23 @@ public static class OutputVariables
         // arrange
         var path = MethodBase.GetCurrentMethod().GetTestDirectory();
         await Sdk.CreateProject(path);
-        var envVars = ("MinVerVersionOverride".ToAltCase(), "2.3.4-alpha-x.5+build.6");
+        var envVars = ("MinVerVersionOverride".ToAltCase(), "2.3.4.5-alpha-x.5+build.6");
 
         // act
         var (_, standardOutput, _) = await Sdk.BuildProject(path, envVars: envVars);
 
         // assert
-        Assert.Contains("MinVer: [output] MinVerVersion=2.3.4-alpha-x.5+build.6", standardOutput, StringComparison.Ordinal);
+        Assert.Contains("MinVer: [output] MinVerVersion=2.3.4.5-alpha-x.5+build.6", standardOutput, StringComparison.Ordinal);
         Assert.Contains("MinVer: [output] MinVerMajor=2", standardOutput, StringComparison.Ordinal);
         Assert.Contains("MinVer: [output] MinVerMinor=3", standardOutput, StringComparison.Ordinal);
         Assert.Contains("MinVer: [output] MinVerPatch=4", standardOutput, StringComparison.Ordinal);
+        Assert.Contains("MinVer: [output] MinVerRevision=5", standardOutput, StringComparison.Ordinal);
         Assert.Contains("MinVer: [output] MinVerPreRelease=alpha-x.5", standardOutput, StringComparison.Ordinal);
         Assert.Contains("MinVer: [output] MinVerBuildMetadata=build.6", standardOutput, StringComparison.Ordinal);
         Assert.Contains("MinVer: [output] AssemblyVersion=2.0.0.0", standardOutput, StringComparison.Ordinal);
-        Assert.Contains("MinVer: [output] FileVersion=2.3.4.0", standardOutput, StringComparison.Ordinal);
-        Assert.Contains("MinVer: [output] InformationalVersion=2.3.4-alpha-x.5+build.6", standardOutput, StringComparison.Ordinal);
-        Assert.Contains("MinVer: [output] PackageVersion=2.3.4-alpha-x.5", standardOutput, StringComparison.Ordinal);
-        Assert.Contains("MinVer: [output] Version=2.3.4-alpha-x.5", standardOutput, StringComparison.Ordinal);
+        Assert.Contains("MinVer: [output] FileVersion=2.3.4.5", standardOutput, StringComparison.Ordinal);
+        Assert.Contains("MinVer: [output] InformationalVersion=2.3.4.5-alpha-x.5+build.6", standardOutput, StringComparison.Ordinal);
+        Assert.Contains("MinVer: [output] PackageVersion=2.3.4.5-alpha-x.5", standardOutput, StringComparison.Ordinal);
+        Assert.Contains("MinVer: [output] Version=2.3.4.5-alpha-x.5", standardOutput, StringComparison.Ordinal);
     }
 }

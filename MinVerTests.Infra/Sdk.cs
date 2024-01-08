8e400ad9fba05f3f13b0f8df045e81b7fa3ff37e
Adam Ralph <adam@adamralph.com>
2024-01-08 13:56:34 +0100

    remove unnecessary using directive

--- MinVerTests.Infra/Sdk.cs
+++ MinVerTests.Infra/Sdk.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
-using System.Runtime;
 using System.Runtime.Loader;
 using System.Threading.Tasks;
 using Microsoft.Extensions.FileSystemGlobbing;

5a57c7c972fb08c0178632b072acdb13af88a29b
Adam Ralph <adam@adamralph.com>
2025-11-21 01:48:13 +0100

    refactor tests infra SDK

--- MinVerTests.Packages/SourceLink.cs
+++ MinVerTests.Packages/SourceLink.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using System.Reflection;
 using MinVerTests.Infra;
 using Xunit;
@@ -12,22 +11,18 @@ public static class SourceLink
     {
         // arrange
         var path = MethodBase.GetCurrentMethod().GetTestDirectory();
-        await Sdk.CreateProject(path, enableSourceLink: true);
-
-        if (string.IsNullOrEmpty(Sdk.Version) ||
-            int.Parse(Sdk.Version.Split(".")[0], NumberStyles.None, CultureInfo.InvariantCulture) < 8)
-        {
-            _ = await Sdk.DotNet(
-                "add package Microsoft.SourceLink.GitHub --version 1.1.1 --package-directory packages", path);
-            _ = await Sdk.DotNet("restore --packages packages", path);
-        }
+        await Sdk.CreateProject(path);
 
         await Git.Init(path);
         await Git.Commit(path);
         var sha = (await Git.GetCommitShas(path)).Single();
 
         var buildMetadata = "build.123";
-        var envVars = ("MinVerBuildMetadata", buildMetadata);
+        (string, string)[] envVars = [
+            ("IncludeSourceRevisionInInformationalVersion", "true"),
+            ("MinVerBuildMetadata", buildMetadata)
+        ];
+
         var expected = Package.WithVersion(0, 0, 0, ["alpha", "0",], 0, "build.123", $".{sha}");
 
         // act

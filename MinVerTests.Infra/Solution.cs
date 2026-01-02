45300ed9f60a37e3949ba191f1a96aba821c6f38
Adam Ralph <adam@adamralph.com>
2026-01-02 10:38:05 +0100

    add pack fixture

--- MinVerTests.Infra/Solution.cs
+++ MinVerTests.Infra/Solution.cs
@@ -1,7 +1,14 @@
 namespace MinVerTests.Infra;
 
-internal static class Solution
+public static class Solution
 {
     public static string GetFullPath(string path) =>
         Path.GetFullPath(Path.Combine(typeof(Solution).Assembly.Location, "../../../../../", path));
+
+    public const string Configuration =
+#if DEBUG
+        "Debug";
+#elif RELEASE
+        "Release";
+#endif
 }
